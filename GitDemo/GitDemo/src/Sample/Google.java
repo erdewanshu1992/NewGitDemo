ecc768d30dcf3951cc6078b6cb444e754e57c955
Yesmadam <erdewanshu1992@gmail.com>
2022-12-17 15:03:48 +0530

    1st

---
+++ GitDemo/GitDemo/src/Sample/Google.java
@@ -0,0 +1,24 @@
+package Sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Google {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		String baseUrl = "http://demo.guru99.com/test/newtours/";
+		String expectedTitle = "Welcome: Mercury Tours";
+		String actualTitle = "";
+		driver.get(baseUrl);
+		actualTitle = driver.getTitle();
+
+		if (actualTitle.contentEquals(expectedTitle)) {
+			System.out.println("Test Passed!");
+		} else {
+			System.out.println("Test Failed");
+		}
+
+	}
+}
---
+++ GitDemo/webMobile/src/main/java/GoogleSample/Google.java
@@ -0,0 +1,24 @@
+package GoogleSample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Google {
+	@Test
+	public void googleTest() {
+		WebDriver driver = new ChromeDriver();
+		String baseUrl = "http://demo.guru99.com/test/newtours/";
+		String expectedTitle = "Welcome: Mercury Tours";
+		String actualTitle = "";
+		driver.get(baseUrl);
+		actualTitle = driver.getTitle();
+
+		if (actualTitle.contentEquals(expectedTitle)) {
+			System.out.println("Test Passed!");
+		} else {
+			System.out.println("Test Failed");
+		}
+	}
+
+}
