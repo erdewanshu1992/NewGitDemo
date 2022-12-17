b38723089514f8101a7c77c2b0772996df003af5
Dewanshu DEv <57786221+erdewanshu1992@users.noreply.github.com>
2022-12-17 22:23:31 +0530

    setup

--- GitDemo/webMobile/src/main/java/sample/AndroidChromeTest.java
+++ GitDemo/webMobile/src/main/java/sample/AndroidChromeTest.java
@@ -20,9 +20,11 @@ public class AndroidChromeTest {
 		options.setDeviceName("vivo");
 		//options.setDeviceName("x8rwh6skrolrjvdy");//Redmi Note 10T 5G
 		//options.setDeviceName("Redmi Note 10T 5G");//Redmi Note 10T 5G
-		options.setChromedriverExecutable("/home/yesmadam/Desktop/Drivers/chromedriver");
+		//options.setChromedriverExecutable("/home/yesmadam/Desktop/Drivers/chromedriver");//yesmadam setup
+		options.setChromedriverExecutable("C:\\Users\\erdew\\Desktop\\Seleniumfiles\\chromedriver.exe");//Asus setup
 		options.setCapability("browserName", "Chrome");
-		driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), options);//Asus setup
+		//driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);//yesmadam setup
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		// driver.get("http://api-stage-aws.yesmadam.co.in/");
 		// driver.get("https://www.yesmadam.com/");
