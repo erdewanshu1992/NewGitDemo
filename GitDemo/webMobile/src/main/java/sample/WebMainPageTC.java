2914303c967e7ca9757b2f232531e040dc8f289f
yesmadam <erdewanshu1992@gmail.com>
2022-12-17 16:28:41 +0530

    TC priority

--- GitDemo/webMobile/src/main/java/sample/WebMainPageTC.java
+++ GitDemo/webMobile/src/main/java/sample/WebMainPageTC.java
@@ -3,7 +3,7 @@ package sample;
 import org.testng.annotations.Test;
 
 public class WebMainPageTC extends AndroidChromeTest {
-	@Test
+	@Test()
 	public void navigate_to_homepage_click_on_SetUrLocation() {
 
 		webMainPage webMainPage = new webMainPage(driver);
